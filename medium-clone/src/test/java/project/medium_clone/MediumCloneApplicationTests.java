e659f614d8602bb680cabdd7e4807e07abd8c4fd
Oconnor Burton <oconnorburton387@gmail.com>
2025-07-20 22:50:51 -0500

    server

---
+++ medium-clone/src/test/java/project/medium_clone/MediumCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package project.medium_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
